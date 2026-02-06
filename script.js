493e6d3cfee566d523aa0c20e40d2e8cdb5c600a
kimon-mgst <boomnodiaoi@gmail.com>
2026-02-07 01:55:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-let currentImg = "";
+let currentImages = {};
 
-function openChara(name, text, img, color) {
+function openChara(name, text, images, color) {
   document.getElementById("modal-name").textContent = name;
   document.getElementById("modal-text").textContent = text;
-  document.getElementById("modal-img").src = img;
+
+  currentImages = images; // ← 今開いてるキャラの画像セット
+  document.getElementById("modal-img").src = images.uniform;
 
   document.documentElement.style.setProperty("--accent", color);
 
   const modal = document.getElementById("chara-modal");
   modal.classList.add("active");
+}
+
+function changeCostume(type) {
+  if (currentImages[type]) {
+    document.getElementById("modal-img").src = currentImages[type];
+  }
 }
 
 function closeChara() {
