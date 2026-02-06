f2177361eadc3fd92697f1f181915a4d729b3034
kimon-mgst <boomnodiaoi@gmail.com>
2026-02-06 23:18:35 +0900

    Implement changeCostume function
    Added functionality to change the character's costume dynamically.

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+let currentImg = "";
+
 function openChara(name, text, img) {
   document.getElementById("modal-name").textContent = name;
   document.getElementById("modal-text").textContent = text;
   document.getElementById("modal-img").src = img;
+  currentImg = img;
   document.getElementById("chara-modal").style.display = "block";
+}
+
+function changeCostume(img) {
+  document.getElementById("modal-img").src = img;
+  currentImg = img;
 }
 
 function closeChara() {
